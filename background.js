41e91910c480c1c7896c3eeb9341e28f2d1d5fe1
Andrew Chopko <andreychopko@gmail.com>
2018-11-12 20:13:04 +0200

    clear comments

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-// Update the declarative rules on install or upgrade.
 chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
